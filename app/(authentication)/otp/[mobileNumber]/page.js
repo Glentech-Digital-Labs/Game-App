8aa8f2cb8abeca0812c32072ecfc73433e80fdeb
RAVI SHANKAR SINGH <ravishankarsingh@RAVIs-MacBook-Pro.local>
2023-06-28 21:38:45 +0530

    changes in otp UI

--- app/(authentication)/otp/[mobileNumber]/page.js
+++ app/(authentication)/otp/[mobileNumber]/page.js
@@ -1,20 +1,23 @@
 "use client"
 import { OtpInput } from "@components"
 import { Circle, YellowButton } from "@components"
-import React, { useState } from "react"
+import React, { useState, useEffect, useRef } from "react"
 import { useParams, useRouter } from "next/navigation"
 import { useDispatch, useSelector } from "react-redux"
 import FetchData from "@utils/Fetcher"
 import { resetUser, setUser } from "/redux/feature/user/userSlice"
 
+// Resend the Otp is still Left
 function Otp() {
   const [otp, setOtp] = useState("")
   const onChange = (value) => setOtp(value)
-  const isOTPSent = true
+  let intervalRef = useRef()
+  const [timer, setTimer] = useState(10)
+  const [otpSend, setSendOtp] = useState(true)
   const userData = useSelector((state) => state.userContext)
   const dispatch = useDispatch()
   const router = useRouter()
-
+  const decreaseNum = () => setTimer((prev) => prev - 1)
   async function submitHandler() {
     const response = await FetchData("punter/sign-up", {
       method: "POST",
@@ -37,10 +40,24 @@ function Otp() {
     console.log("Otp wale ka response", response)
   }
 
+  useEffect(() => {
+    if (timer >= 1) {
+      intervalRef.current = setInterval(() => {
+        decreaseNum()
+      }, 1000)
+    } else {
+      setSendOtp(false)
+    }
+    return () => {
+      clearTimeout(intervalRef.current)
+    }
+  }, [timer])
+
   return (
     <div className="tw-flex tw-flex-col ">
       <p className="tw-text-xl tw-font-semibold tw-mt-6 tw-self-center">
-        OTP send to +91 9166186443
+        <span>{`OTP send to ${userData.phoneNumber}`}</span>
+        9166186443`
       </p>
       <p className="tw-text-lg tw-font-semibold tw-mt-6 tw-self-center">
         Enter the OTP you Received
@@ -49,9 +66,10 @@ function Otp() {
         <OtpInput value={otp} valueLength={4} onChange={onChange} />
       </div>
       <div className="tw-text-center tw-my-8">
-        {isOTPSent ? (
+        {otpSend ? (
           <>
-            Resend OTP in <span className="tw-text-red-500">10 second</span>
+            Resend OTP in{" "}
+            <span className="tw-text-red-500">{`  ${timer} second`}</span>
           </>
         ) : (
           <p className="tw-text-red-500">Resend</p>
