e56ec23b5afc40e7b5021305ccb24502178a218d
RAVI SHANKAR SINGH <ravishankarsingh@RAVIs-MacBook-Pro.local>
2023-07-06 18:25:10 +0530

    add the transition of uI better

--- app/(account)/transactions/layout.js
+++ app/(account)/transactions/layout.js
@@ -1,13 +1,19 @@
+"use client"
+import { useRouter } from "next/navigation"
 import { AiOutlineLeft } from "/utils/Icons"
-export const metadata = {
+const metadata = {
   title: "OTP ",
   description: "Making Gaming App",
 }
 
 function RootLayout({ children }) {
+  const router = useRouter()
   return (
     <section>
-      <div className="header tw-flex tw-items-center tw-mb-3 ">
+      <div
+        className="header tw-flex tw-items-center "
+        onClick={() => router.back()}
+      >
         <AiOutlineLeft fontSize={32} />
       </div>
       {children}
--- components/Bank/Transactions.server.js
+++ components/Bank/Transactions.server.js
@@ -3,34 +3,34 @@ import { Input } from "@components/common/InputComponent"
 
 function Header() {
   return (
-    <div className="tw-flex tw-mx-2 ">
+    <div className="tw-flex ">
       <div
-        className="tw-bg-[#2B2B31] tw-border-2 tw-border-black tw-flex tw-items-center  tw-mr-2  tw-w-[45%] tw-rounded-t-xl"
+        className="tw-bg-[#2B2B31]  tw-border-2 tw-border-[#2B2B31] tw-flex tw-items-center  tw-mr-2  tw-w-[50%] tw-rounded-xl"
         style={{ borderStyle: "outset" }}
       >
         <div
-          className="tw-bg-[#3B3B42] tw-rounded-full tw-p-2 tw-flex tw-my-2 "
+          className="tw-bg-[#3B3B42] tw-rounded-full tw-p-2 tw-flex tw-my-2 tw-ml-2 "
           style={{ borderStyle: "outset" }}
         >
-          <BiWallet fontSize={32} className="tw-self-center" />
+          <BiWallet fontSize={18} className="tw-self-center" />
         </div>
-        <div className="tw-flex tw-flex-col tw-items-center tw-ml-2">
-          Total Deposit
+        <div className="tw-flex tw-flex-col tw-items-center tw-pl-2 ">
+          <span className="tw-text-12px">Total Deposit</span>
           <span className="tw-font-semibold">7500</span>
         </div>
       </div>
       <div
-        className="tw-bg-[#2B2B31] tw-border-2 tw-border-black tw-flex tw-justify-center tw-items-center tw-w-[55%] tw-rounded-xl"
+        className="tw-bg-[#2B2B31] tw-border-2 tw-border-[#2B2B31] tw-flex  tw-items-center tw-w-[50%] tw-rounded-xl"
         style={{ borderStyle: "outset" }}
       >
         <div
-          className="tw-bg-[#3B3B42] tw-rounded-full  tw-p-2"
+          className="tw-bg-[#3B3B42] tw-rounded-full  tw-p-2 tw-ml-2"
           style={{ borderStyle: "outset" }}
         >
-          <BiWallet fontSize={32} />
+          <BiWallet fontSize={18} />
         </div>
-        <div className="tw-flex tw-flex-col  tw-ml-2">
-          Total WithDrawals
+        <div className="tw-flex tw-flex-col  tw-ml-2 ">
+          <span className="tw-text-12px">Total WithDrawals</span>
           <span className="tw-font-semibold">7500</span>
         </div>
       </div>
@@ -40,25 +40,28 @@ function Header() {
 function InputComponent() {
   return (
     <>
-      <div className="tw-col-span-1 tw-grid tw-grid-rows-2 ">
-        {/* Need to change the color of border and Calender which is looking black */}
+      <div className="tw-col-span-1 tw-grid tw-grid-rows-2 tw-min-w-full">
         <Input
           type={"date"}
           label={"From"}
+          style={{ minWidth: "100%" }}
           className={"tw-border-2  tw-border-gray-600 "}
         />
+
         <Input
           type={"text"}
           value={"Deposit"}
           label={"Transition Type"}
           setValue={() => {}}
+          style={{ minWidth: "100%" }}
           className={"tw-border-2  tw-border-gray-600 "}
         />
       </div>
-      <div className="tw-col-span-1 tw-grid tw-grid-rows-2 ">
+      <div className="tw-col-span-1 tw-grid tw-grid-rows-2 tw-min-w-full">
         <Input
           type={"date"}
           label={"To"}
+          style={{ minWidth: "100%" }}
           className={"tw-border-2  tw-border-gray-600 "}
         />
         <Input
@@ -66,6 +69,7 @@ function InputComponent() {
           value={"Deposit"}
           label={"Status"}
           setValue={() => {}}
+          style={{ minWidth: "100%" }}
           className={"tw-border-2  tw-border-gray-600"}
         />
       </div>
